2fe0f106f7bea31a69342bb8d27b1602bf49419f
dmora219 <dmora14633@gmail.com>
2023-11-13 10:35:34 -0600

    did first 5 challenges so far

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,8 @@
 // this is your main.js script
+let challenge = "30 Days Of Javascript";
+console.log(challenge)
+console.log(challenge.length)
+challenge = challenge.toUpperCase();
+console.log(challenge);
+challenge = challenge.toLowerCase();
+console.log(challenge);
